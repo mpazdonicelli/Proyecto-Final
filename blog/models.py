4b4a2fe8724f03c3e74b105080070f037865f4b7
mpazdonicelli <pachudonicelli@hotmail.com>
2023-05-21 09:58:43 -0300

    Creado modelo reseña (=artículo)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Reseña (models.Model):
+    titulo = models.CharField(max_length=256)
+    subtitulo = models.CharField(max_length=256)
+    cuerpo = models.TextField()
+    autor = models.CharField(max_length=256)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return f"{self.titulo} \n {self.subtitulo}"
